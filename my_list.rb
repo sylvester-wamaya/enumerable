8bb1733c398e3a852f2fcbe70b34dd70792b9853
jic <ji_camargo@yahoo.com>
2023-07-27 17:01:47 -0500

    fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -14,12 +14,8 @@ end
 
 
 list = MyList.new(1, 2, 3, 4)
-puts(list.all? {|e| e < 5})
-puts(list.all? {|e| e > 5})
-puts(list.any? {|e| e == 2})
-puts(list.any? {|e| e == 5})
-puts(list.filter {|e| e.even?})
-
-
-
-
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))

fb7687c04bfaab5bb1d9bb9529fff15151f57110
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-07-28 00:35:11 +0300

    Add test code

--- my_list.rb
+++ my_list.rb
@@ -13,5 +13,13 @@ class MyList
 end
 
 
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? {|e| e < 5})
+puts(list.all? {|e| e > 5})
+puts(list.any? {|e| e == 2})
+puts(list.any? {|e| e == 5})
+puts(list.filter {|e| e.even?})
+
+
 
 
